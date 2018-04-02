ea74fbc16a7c2939553ec898141e63d11ef7d8ef
Remm97 <eldabaghr@student.wpunj.edu>
2018-04-02 15:12:12 -0400

    Add files via upload

--- Wavefunction...4.2.18.py
+++ Wavefunction...4.2.18.py
@@ -7,7 +7,7 @@ ax = plt.axes(xlim=(0, 500), ylim=(-0.5, .5))
 line, = ax.plot([], [], lw=2)
 
 def PIB_Func(x, n, L):
-    psi_n = np.sqrt(2./L)*np.sin(2*np.pi*x/L)+np.sqrt(2./L)*np.sin(5*np.pi*x/L)+np.sqrt(2./L)*np.sin(7*np.pi*x/L)
+    psi_n = np.sqrt(2./L)*np.sin(n*np.pi*x/L) #THIS IS OUR WAVE SKELETON
     return psi_n
 def PIB_En(n, L):
     En = (n*n * np.pi*np.pi)/(2*L*L)
@@ -20,27 +20,22 @@ def PIB_Time(n, L, t):
     return phi_n_t
 
 L = 500.
-xt = np.linspace(0, L, 2000)
-psi_exp = np.zeros(len(xt),dtype=complex)
-n=1
+x = np.linspace(0, L, 2000)
+psi_exp = np.zeros(len(x),dtype=complex)
+#n=5
 
 
-
-psi = PIB_Func(x, n, L)
+psi = PIB_Func(x, 3, L) + PIB_Func(x, 6, L) #THIS ADDS 2 FUNCTIONS TOGETHER TO MAKE A SUPER POSTIION
 p=psi*psi
-plt.plot(x, np.real(psi), 'purple', x,p, 'red') #P,'green', x, psi_1, 'orange', z,'blue') 
-plt.show()
+plt.plot(x, np.real(psi), 'purple',) #x,p, 'red') #THIS STARED OUT P IS THE PROBABLITY
 
-nt = np.linspace(1,200,200)
-cn = FourierAnalysis(x, n, L)
-
+#nt = np.linspace(1,200,200)
+#cn = FourierAnalysis(x, n, L)
 
 
-def init():
-    line.set_data([], [])
-    return line,
-"""
-Created on Mon Apr  2 14:52:43 2018
 
-@author: eldabaghr
-"""
+
+
+#def init():
+#    line.set_data([], [])
+#    return line,
